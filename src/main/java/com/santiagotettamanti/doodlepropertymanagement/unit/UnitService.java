64065146231f57cfd277f6c0152cbc3a4ab246ef
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-07-02 14:37:57 -0500

    Created method to get unit by ID

--- src/main/java/com/santiagotettamanti/doodlepropertymanagement/unit/UnitService.java
+++ src/main/java/com/santiagotettamanti/doodlepropertymanagement/unit/UnitService.java
@@ -2,6 +2,25 @@ package com.santiagotettamanti.doodlepropertymanagement.unit;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class UnitService {
+    public ArrayList<Unit> units = new ArrayList<>();
+
+    public boolean addUnit(Unit unit) {
+        units.add(unit);
+        return true;
+    }
+
+    public Unit getUnitById(int id) {
+        for (int i = 0; i < units.size(); i++) {
+            Unit currentUnit = units.get(i);
+            int unitId = currentUnit.getUnitId();
+            if (unitId == id) {
+                return currentUnit;
+            }
+        }
+        return null;
+    }
 }
