4f808ce60635204f914a014b54c2042bf1a58ee1
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-07-05 21:09:36 -0500

    Created method to get property by ID

--- src/main/java/com/santiagotettamanti/doodlepropertymanagement/property/PropertyService.java
+++ src/main/java/com/santiagotettamanti/doodlepropertymanagement/property/PropertyService.java
@@ -1,7 +1,27 @@
 package com.santiagotettamanti.doodlepropertymanagement.property;
 
+import com.santiagotettamanti.doodlepropertymanagement.floorplan.FloorPlan;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class PropertyService {
+    private ArrayList<Property> properties = new ArrayList<>();
+
+    public boolean addProperty(Property property) {
+        properties.add(property);
+        return true;
+    }
+
+    public Property getPropertyById(int id) {
+        for(int i=0; i<properties.size(); i++) {
+            Property currentProperty = properties.get(i);
+            currentProperty.getPropertyId();
+            if(currentProperty.getPropertyId()==id) {
+                return currentProperty;
+            }
+        }
+        return null;
+    }
 }
