d509319e767db3aba28b489e9873a0ee7c13c520
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-07-05 20:43:38 -0500

    Created method to get tenant by ID

--- src/main/java/com/santiagotettamanti/doodlepropertymanagement/tenant/TenantService.java
+++ src/main/java/com/santiagotettamanti/doodlepropertymanagement/tenant/TenantService.java
@@ -2,6 +2,25 @@ package com.santiagotettamanti.doodlepropertymanagement.tenant;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class TenantService {
+    private ArrayList<Tenant> tenants = new ArrayList<>();
+
+    public boolean addTenant(Tenant tenant) {
+        tenants.add(tenant);
+        return true;
+    }
+
+    public Tenant getTenantById(int id) {
+        for(int i=0; i<tenants.size(); i++) {
+            Tenant currentTenant = tenants.get(i);
+            int tenantId = currentTenant.getTenantId();
+            if(tenantId == id) {
+                return currentTenant;
+            }
+        }
+        return null;
+    }
 }
