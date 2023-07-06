eca23f000cb3ce369f93605c3be6dfeb480e216b
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-07-05 20:59:51 -0500

    Created method to get floor plan by ID

--- src/main/java/com/santiagotettamanti/doodlepropertymanagement/floorplan/FloorPlanService.java
+++ src/main/java/com/santiagotettamanti/doodlepropertymanagement/floorplan/FloorPlanService.java
@@ -2,6 +2,25 @@ package com.santiagotettamanti.doodlepropertymanagement.floorplan;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class FloorPlanService {
+    private ArrayList<FloorPlan> floorPlans = new ArrayList<>();
+
+    public boolean addFloorPlan(FloorPlan floorPlan) {
+        floorPlans.add(floorPlan);
+        return true;
+    }
+
+    public FloorPlan getFloorPlanById(int id) {
+        for(int i=0; i<floorPlans.size(); i++) {
+            FloorPlan currentFloorPlan = floorPlans.get(i);
+            int FloorPlanId = currentFloorPlan.getFloorPlanId();
+            if(FloorPlanId==id) {
+                return currentFloorPlan;
+            }
+        }
+        return null;
+    }
 }
