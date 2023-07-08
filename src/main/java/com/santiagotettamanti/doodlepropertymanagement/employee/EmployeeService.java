5aaa2700eb697e489445af130c0587e7f147ca2b
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-07-08 16:54:16 -0400

    Created methods to delete by ID properties, floor plans, and employees

--- src/main/java/com/santiagotettamanti/doodlepropertymanagement/employee/EmployeeService.java
+++ src/main/java/com/santiagotettamanti/doodlepropertymanagement/employee/EmployeeService.java
@@ -15,12 +15,21 @@ public class EmployeeService {
 
     public Employee getEmployeeById(int id) {
         for(int i=0; i<employees.size();i++) {
-            Employee currentEmployee = employees.get(i);  //save the current employee from the loop in a variable "currentEmployee"
+            Employee currentEmployee = employees.get(i);        //save the current employee from the loop in a variable "currentEmployee"
             int employeeId = currentEmployee.getEmployeeId();   //find the ID of that employee with the method created in the Employee class (the getter)
-            if(employeeId==id) {                         // check if the employeeId is the same as the one provided in the parameter
-                return currentEmployee;                 // return that employee if ^ is true
+            if(employeeId==id) {                               // check if the employeeId is the same as the one provided in the parameter
+                return currentEmployee;                        // return that employee if ^ is true
             }
         }
-        return null;                                    // otherwise return null
+        return null;                                           // otherwise return null
+    }
+
+    public Employee deleteEmployeeById(int id) {
+        for(int i=0; i<employees.size(); i++) {
+            if(employees.get(i).getEmployeeId()==id) {
+                return employees.remove(i);
+            }
+        }
+        return null;
     }
 }
