ccfa511791ea17dbf370a295358d5058673585cc
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-07-22 11:57:29 -0500

    Updated test methods for employee class

--- src/test/java/com/santiagotettamanti/doodlepropertymanagement/employee/EmployeeServiceTest.java
+++ src/test/java/com/santiagotettamanti/doodlepropertymanagement/employee/EmployeeServiceTest.java
@@ -2,27 +2,52 @@ package com.santiagotettamanti.doodlepropertymanagement.employee;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeServiceTest {
+    EmployeeService employeeService = new EmployeeService();
 
     @Test
     void addEmployee() {
+        Employee newEmployee = new Employee();
+        newEmployee.setFirstName("Jaimicho");
+        newEmployee.setLastName("Moncalico");
+        newEmployee.setPhoneNumber("6825970042");
+        newEmployee.setEmail("jaimicho.moncalico@live.ec");
+        Employee employee = employeeService.addEmployee(newEmployee);
+        assertEquals(1, employee.getEmployeeId());
     }
 
     @Test
     void getEmployees() {
+        addEmployee();
+        ArrayList<Employee> employees = employeeService.getEmployees();
+        assertEquals(1, employees.size());
     }
 
     @Test
     void getEmployeeById() {
+        addEmployee();
+        Employee employee = employeeService.getEmployeeById(1);
+        assertEquals(1, employee.getEmployeeId());
     }
 
     @Test
     void deleteEmployeeById() {
+        addEmployee();
+        Employee employee = employeeService.deleteEmployeeById(1);
+        assertEquals(1, employee.getEmployeeId());
+        assertEquals(0, employeeService.getEmployees().size());
     }
 
     @Test
     void updateEmployee() {
+        addEmployee();
+        Employee newEmployee = new Employee();
+        newEmployee.setLastName("Monclave");
+        Employee employee = employeeService.updateEmployee(newEmployee, 1);
+        assertEquals("Monclave", employee.getLastName());
     }
 }
