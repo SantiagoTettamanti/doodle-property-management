543b5bd41c94162c06359dc45b9500f1e9d50693
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-08-02 20:51:09 -0500

    Ran test on Tenant

--- src/test/java/com/santiagotettamanti/doodlepropertymanagement/tenant/TenantServiceTest.java
+++ src/test/java/com/santiagotettamanti/doodlepropertymanagement/tenant/TenantServiceTest.java
@@ -2,12 +2,31 @@ package com.santiagotettamanti.doodlepropertymanagement.tenant;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TenantServiceTest {
     TenantService tenantService = new TenantService();
 
     @Test
     void addTenant() {
-        Tenant tenant = new Tenant();
+        Tenant newTenant = new Tenant();
+        newTenant.setFirstName("Bob");
+        newTenant.setLastName("Dylan");
+        newTenant.setUnitId(1);
+        newTenant.setMoveOutDate(LocalDate.of(2023, 1, 25));
+        newTenant.setMoveInDate(LocalDate.of(2022, 1, 25));
+        newTenant.setEmail("bob.dylan@gmail.com");
+        Tenant tenant = tenantService.addTenant(newTenant);
+        assertEquals(1, tenant.getTenantId());
+    }
 
+    @Test
+    void getTenant() {
+        addTenant();
+        ArrayList<Tenant> tenants = tenantService.getTenants();
+        assertEquals(1, tenants.size());
     }
 }
